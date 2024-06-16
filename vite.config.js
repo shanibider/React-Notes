096c22bd47c43e82985676d1a3c22f69bee7e30b
Shani Bider <shanibider@gmail.com>
2024-06-16 13:51:32 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    base: '/React-Notes/',
     outDir: 'dist', // Default output directory
   },
 });
